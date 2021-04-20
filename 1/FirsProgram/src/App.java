f16ae803b8086307655c5dbda1e61ddce66e9b7b
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-20 21:09:06 +0200

    Update App.java

--- 1/FirsProgram/src/App.java
+++ 1/FirsProgram/src/App.java
@@ -1,12 +1,21 @@
 // Toto je je první soubor
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        int x = 123;
-        double y = 3.14;
-        boolean z = true;
-        char symbol = '.';
-        String name = "Peter";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt(); scanner.nextLine();
+        
+        System.out.println("What is your favourite food? ");
+        String food = scanner.nextLine();
 
         System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You like "+food);
     }
 }
