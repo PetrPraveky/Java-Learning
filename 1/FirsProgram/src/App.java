09e9d8829875719cfef3a1269dcd89cd260cf8ce
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-20 20:56:41 +0200

    Update App.java

--- 1/FirsProgram/src/App.java
+++ 1/FirsProgram/src/App.java
@@ -1,7 +1,12 @@
 // Toto je je první soubor
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("I like pizza");
+        int x = 123;
+        double y = 3.14;
+        boolean z = true;
+        char symbol = '.';
+        String name = "Peter";
+
+        System.out.println("Hello "+name);
     }
 }
