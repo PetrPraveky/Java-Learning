159489f9fad367c32fce43c2ba7cdaf693792dbb
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-21 18:48:30 +0200

    Update App.java

--- 1/FirsProgram/src/App.java
+++ 1/FirsProgram/src/App.java
@@ -1,18 +1,28 @@
 // Toto je je první soubor
 // https://www.youtube.com/watch?v=xk4_1vDrzzo
-import java.util.ArrayList;
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        ArrayList<String> food = new ArrayList<String>();
 
-        food.add("pizza");
-        food.add("hamburger");
-        food.add("hotdog");
+        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
 
-        food.clear();
+        ArrayList<String> bakeryList = new ArrayList<>();
+        bakeryList.add("pasta");
+        bakeryList.add("garlic bread");
+        bakeryList.add("donuts");
+        groceryList.add(bakeryList);
 
-        for (int i=0; i<food.size(); i++) {
-            System.out.println(food.get(i));
-        }
+        ArrayList<String> produceList = new ArrayList<>();
+        produceList.add("tomato");
+        produceList.add("zucchini");
+        produceList.add("peppers");
+        groceryList.add(produceList);
+
+        ArrayList<String> drinkList = new ArrayList<>();
+        drinkList.add("soda");
+        drinkList.add("coffee");
+        groceryList.add(drinkList);
+
+        System.out.println(groceryList.get(0).get(0));
     }   
 }
