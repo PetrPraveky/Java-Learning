181918f4d764562257c7184283cdd7ca38c793b9
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-21 20:04:51 +0200

    Added new file with OOP

--- 1/FirsProgram/src/App.java
+++ 1/FirsProgram/src/App.java
@@ -1,28 +1,8 @@
 // Toto je je první soubor
 // https://www.youtube.com/watch?v=xk4_1vDrzzo
-import java.util.*;
+// import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
-
-        ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
-
-        ArrayList<String> bakeryList = new ArrayList<>();
-        bakeryList.add("pasta");
-        bakeryList.add("garlic bread");
-        bakeryList.add("donuts");
-        groceryList.add(bakeryList);
-
-        ArrayList<String> produceList = new ArrayList<>();
-        produceList.add("tomato");
-        produceList.add("zucchini");
-        produceList.add("peppers");
-        groceryList.add(produceList);
-
-        ArrayList<String> drinkList = new ArrayList<>();
-        drinkList.add("soda");
-        drinkList.add("coffee");
-        groceryList.add(drinkList);
-
-        System.out.println(groceryList.get(0).get(0));
-    }   
+        System.out.println("Hello world");
+    }
 }
---
+++ 1/FirstProgram - more calsses/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Car myCar1 = new Car();
+        Car myCar2 = new Car();
+
+        System.out.println(myCar1.make);
+        System.out.println(myCar1.model);
+        System.out.println();
+        System.out.println(myCar2.make);
+        System.out.println(myCar2.model);
+    }
+}
