b2e8780f0db56a59a5d15013777d5d08196dba28
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-21 17:56:49 +0200

    Some progress and creating some new files

--- 1/FirsProgram/src/App.java
+++ 1/FirsProgram/src/App.java
@@ -1,16 +1,7 @@
 // Toto je je první soubor
+// https://www.youtube.com/watch?v=xk4_1vDrzzo
 public class App {
     public static void main(String[] args) throws Exception {
-        int age = -5;
-
-        if(age>=75) {
-            System.out.println("Ok boomer!");
-        }
-        else if(age>=18) {
-            System.out.println("You are an adult!");
-        }
-        else {
-            System.out.println("You are not an adult!");
-        }
+        
     }
 }
