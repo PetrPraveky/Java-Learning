874a17872890ee179bf138dd8825f545d4ff64a6
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-21 18:10:45 +0200

    Update App.java

--- 1/FirsProgram/src/App.java
+++ 1/FirsProgram/src/App.java
@@ -2,6 +2,17 @@
 // https://www.youtube.com/watch?v=xk4_1vDrzzo
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        String [][] cars = {
+            {"Camero", "Corvette", "Silverado"},
+            {"Mustang", "Ranger", "F-150"}, 
+            {"Ferrari", "Lambo", "Tesla"}
+        };
+
+        for (int i=0; i<cars.length; i++) {
+            System.out.println();
+            for(int j=0; j<cars[i].length; j++) {
+                System.out.print(cars[i][j]+" ");
+            }
+        }
     }
 }
