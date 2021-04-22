04c24f931e0c187ae2e669ff15845f8a553671e9
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-22 10:56:33 +0200

    Still learning OOP

--- 1/FirstProgram - more calsses/src/App.java
+++ 1/FirstProgram - more calsses/src/App.java
@@ -1,12 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Car myCar1 = new Car();
-        Car myCar2 = new Car();
 
-        System.out.println(myCar1.make);
-        System.out.println(myCar1.model);
-        System.out.println();
-        System.out.println(myCar2.make);
-        System.out.println(myCar2.model);
+        // Human human1 = new Human("Rick", 65, 70);
+        Human human2 = new Human("Liz", 18, 45);
+
+        // System.out.println(human2.name);
+        human2.eat();
     }
 }
