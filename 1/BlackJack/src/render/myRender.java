6112ecb1c0ba9d94852f06b52a075dd9a40ef4e9
PetrPraveky <56596287+PetrPraveky@users.noreply.github.com>
2021-04-23 17:22:26 +0200

    Update 2

--- 1/BlackJack/src/render/myRender.java
+++ 1/BlackJack/src/render/myRender.java
@@ -7,18 +7,25 @@ public class myRender extends JFrame {
     myRenderCards card;
 
     public myRender() {
+        renderMainFrame();
+
+        loadPanels();
+
+        this.setLocation(0, 0);
+        this.setVisible(true);
+    }
+
+    void renderMainFrame() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setUndecorated(true);
         this.setLocationRelativeTo(null);
-        this.setBackground(new Color(0x141414));
+        this.setBackground(new Color(0x141414));        
+    }
 
+    void loadPanels() {
         card = new myRenderCards();
         this.add(card);
         this.pack();
-
-        this.setLocation(0, 0);
-
-        this.setVisible(true);
     }
 }
--- 1/BlackJack/src/render/myRenderCards.java
+++ 1/BlackJack/src/render/myRenderCards.java
@@ -1,18 +1,31 @@
 package render;
-
-import java.awt.*;
-import javax.swing.*;
-
+//Balíček pro render karet
+import java.awt.*; //Přidání modulu awt
+import javax.swing.*; //Přidání mudlu swing
+//Hlavní třída
 public class myRenderCards extends JPanel{
-    Image image;
+    Image cardDeck; //Vytvoření obrázku pro balíček karet
 
     public myRenderCards() {
-        image = new ImageIcon("D:/Github/Java-Learning/1/BlackJack/res/.cards/cardStackBig-back1.png").getImage();
-        this.setPreferredSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+        cardDeck = new ImageIcon("D:/Github/Java-Learning/1/BlackJack/res/.cards/cardStackBig-back1.png").getImage(); //Přidání textury pro balíček karet
+        this.setPreferredSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize())); //Vykreslení okna
     }
-
+    //Funkce pro render
     public void paint(Graphics g) {
-        Graphics2D g2D = (Graphics2D) g;
-        g2D.drawImage(image, 0, 0, 128, 128, null);
+        Graphics2D g2D = (Graphics2D) g; //Zadání rederu
+        //Vykreslení balíčku karet
+        g2D.drawImage(
+            cardDeck, //Textura
+            0, //X Pozice
+            0, //Y Pozice
+            getImageMult(cardDeck.getWidth(null), 2), //Velikost textury po ose X
+            getImageMult(cardDeck.getHeight(null), 2), //Velikost textury po ose Y
+            null //null
+            );
+    }
+    //Funkce pro vypočítání výsledné velikosti textury
+    int getImageMult(int baseLenght, int multipl) {
+        int Lenght = baseLenght*multipl; //Vypočítání velikosti
+        return Lenght; //Vrácení hodnoty
     }
 }
